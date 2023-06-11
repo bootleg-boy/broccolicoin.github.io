c91467a0fa967d6445703d8d93b0d7029c7b9c27
bootleg-boy <102448813+bootleg-boy@users.noreply.github.com>
2023-06-11 18:21:23 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    // Remove the 'images' property or any other invalid experimental options
+  },
+  // Add the following line to enable static HTML export
+  output: {
+    export: true
+  }
+}
 
 module.exports = nextConfig
